abacc6d89f99e06d5f50b74891cdd0cd413f4399
Madeleine Wood <madeleinecwood@gmail.com>
2020-09-23 21:08:19 +0100

    added lab

---
+++ week_04/day_03/week_04_day_03_full_stack_list_lab-master/controllers/animals_controllers.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect
+
+from models.animals import Animals
+
+from flask import Blueprint
+
+from repositories import animals_repo
+
+animals_blueprint = Blueprint("animals", __name__)
+
+@animals_blueprint.route("/animals") # setting a route
+def animals():
+    animals = animals_repo.select_all()
+    print(animals)
+    return render_template("animals/index.html", all_animals=animals)
+
+@animals_blueprint.route("/animals/<id>/delete", methods=["POST"])
+def delete(id):
+    animals_repo.delete(id)
+    return redirect("/animals")
+
+@animals_blueprint.route("animals/new")
+def new_animal():
+    all_animals = animals_repo.select_all()
+    return render_template("animals/new.html", all_animals=all_animals) 
+
